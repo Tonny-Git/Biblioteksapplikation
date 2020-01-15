9822da1953fcfe288004e0c2579cfd3f4b798df9
Tonny <Tonny.Abrahamsson@hyhs.se>
2020-01-15 12:00:37 +0100

    added attributes to book/booklist

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Book {
+    private String title;
+    private String author;
+    private String description;
+    private boolean isAvailable = true;
+
+    public Book(String title, String author, String description) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+    }
 }
--- src/com/company/BookList.java
+++ src/com/company/BookList.java
@@ -1,4 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class BookList {
+    ArrayList<Book> books = new ArrayList<>();
 }
