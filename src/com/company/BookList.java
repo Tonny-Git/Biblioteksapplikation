aad2aabfc31650f90668478bac60df0e09127ef2
Tonny <Tonny.Abrahamsson@hyhs.se>
2020-01-22 22:06:11 +0100

    kortar ner kåd mängd

--- src/com/company/BookList.java
+++ src/com/company/BookList.java
@@ -21,6 +21,7 @@ public class BookList {
         seeBookAttributes(loggedInPerson);
     }
 
+    //Ta Bort senare
     private int userIntSelection() {
         int answer;
         while (true) {
@@ -35,45 +36,28 @@ public class BookList {
     }
 
     private void seeBookAttributes(Account loggedInPerson) {
-        while (true) {
-            System.out.println("Select a book to see description/option or press 0 to exit");
-            int bookChoice = userIntSelection();
-            if (bookChoice == 0) {
-                return;
-            }
-            try {
-                System.out.println(books.get(bookChoice-1));
-            } catch (Exception e) {
-                System.out.println("This book dosen't exits");
-                continue;
-            }
-            bookOptions(loggedInPerson, bookChoice);
-            return;
+        System.out.println("Select a book to see description/option or press 0 to exit");
+        int bookChoice = MethodUtility.intSelectionArray(books.size());
+        if (bookChoice != -1) {
+            System.out.println(books.get(bookChoice));
+            bookOptions(loggedInPerson, books.get(bookChoice));
         }
     }
 
-    private void bookOptions(Account loggedInPerson, int bookChoice) {
-        boolean loop = true;
-        while (loop) {
+    private void bookOptions(Account loggedInPerson, Book book) {
+        if (loggedInPerson instanceof Admin) {
+            return;
+        }
+        while (true) {
             System.out.println("What do you want to do?");
-            if (loggedInPerson instanceof Admin) {
-                System.out.println("[1] Remove the book");
-            } else  {
-                System.out.println("[1] Rent the book");
-            }
+            System.out.println("[1] Rent the book");
             System.out.println("[0] Return to menu");
             String answer = scanner.nextLine();
 
             switch (answer) {
                 case "1":
-                    if (loggedInPerson instanceof Admin) {
-                        removeOldBook(bookChoice-1);
-                    } else if (loggedInPerson instanceof User){
-                        User person = (User)loggedInPerson;
-                        rentBook(person, bookChoice);
-                    }
-                    loop = false;
-                    break;
+                    rentBook((User) loggedInPerson, book);
+                    return;
                 case "0":
                     return;
                 default:
@@ -105,11 +89,11 @@ public class BookList {
         }
     }
 
-    public void rentBook(User loggedInPerson, int bookChoice) {
-        if (books.get(bookChoice-1).isAvailable()) {
-            loggedInPerson.getBorrowedBooks().add(books.get(bookChoice-1));
-            books.get(bookChoice-1).setAvailable(false);
-            System.out.println("You rented the book: " + books.get(bookChoice-1).getTitle());
+    public void rentBook(User loggedInPerson, Book book) {
+        if (book.isAvailable()) {
+            loggedInPerson.getBorrowedBooks().add(book);
+            book.setAvailable(false);
+            System.out.println("You rented the book: " + book.getTitle());
         } else {
             System.out.println("This book is already rented!");
         }
@@ -122,58 +106,43 @@ public class BookList {
             for (int i = 0; i < loggedInPerson.getBorrowedBooks().size(); i++) {
                 System.out.println(i+1 + " " +loggedInPerson.getBorrowedBooks().get(i).getTitle());
             }
-            System.out.println("Select a book to retun or press 0 to go back");
-
-            try {
-                int answer = Integer.parseInt(scanner.nextLine());
-                if (answer == 0)
-                    return;
-                loggedInPerson.getBorrowedBooks().get(answer-1).setAvailable(true);
-                loggedInPerson.getBorrowedBooks().remove(answer-1);
-            } catch (Exception e) {
-                System.out.println("This is wrong input!");
+            System.out.println("Select a book to return or press 0 to go back");
+            int answer = MethodUtility.intSelectionArray(loggedInPerson.getBorrowedBooks().size());
+            if (answer != -1) {
+                loggedInPerson.getBorrowedBooks().get(answer).setAvailable(true);
+                loggedInPerson.getBorrowedBooks().remove(answer);
             }
         }
-
     }
 
     public void searchAfterBook(Account loggedInPerson) {
         System.out.println("Enter title or author of an book");
         String answer = scanner.nextLine().toLowerCase();
         ArrayList<Book> booksFound = new ArrayList<>();
-        ArrayList<Integer> posistion = new ArrayList<>();
 
-        for (int i = 0; i < books.size(); i++) {
-            if (answer.equals(books.get(i).getTitle().toLowerCase()) || answer.equals(books.get(i).getAuthor().toLowerCase())) {
-                booksFound.add(books.get(i));
-                posistion.add(i);
+        int i = 1;
+        for (Book book : books) {
+            if (answer.equals(book.getTitle().toLowerCase()) || answer.equals(book.getAuthor().toLowerCase())) {
+                booksFound.add(book);
+                System.out.println(i+1 + " " + book.getTitle());
+                i++;
             }
         }
 
         if (booksFound.size() ==  0)
             System.out.println("No Books found");
         else if (booksFound.size() == 1)
-            bookOptions(loggedInPerson, posistion.get(0));
+            bookOptions(loggedInPerson, booksFound.get(0));
         else {
             System.out.println("Select a book or press 0 to exit");
-            for (int i = 0; i < booksFound.size(); i++) {
-                booksFound.get(i).getTitle();
-                System.out.println(i+1 + " " + booksFound.get(i).getTitle());
-            }
-
-            int numberAnswer = userIntSelection();
-
-            if (numberAnswer-1 >= 0 && numberAnswer < posistion.size()) {
-                bookOptions(loggedInPerson, posistion.get(numberAnswer));
-            } else if (numberAnswer == 0) {
-                return;
-            } else {
-                System.out.println("Not a valid input!");
+            int bookAnswer = MethodUtility.intSelectionArray(booksFound.size());
+            if (bookAnswer != -1) {
+                bookOptions(loggedInPerson, booksFound.get(bookAnswer));
             }
         }
     }
 
-
+    //Forsätt härifrån
     // Working on fixing old methods
     public ArrayList<Book> searchThroughArray (String option) {
         ArrayList<Book> booksFound = new ArrayList<>();
